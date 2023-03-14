0d8603f4fe600966f9e8c82cc5b56a56340f69b6
Ryan Collins <rlcollins@g.harvard.edu>
2023-03-13 22:07:54 -0400

    Begin working through VEP annotation

--- shell/annotate_vcfs.sh
+++ shell/annotate_vcfs.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-###############################
-#    RAS Modifiers Project    #
-###############################
+################################
+#    EGFR Modifiers Project    #
+################################
 
-# Copyright (c) 2022-Present Ryan L. Collins and the Van Allen/Gusev/Haigis Laboratories
+# Copyright (c) 2023-Present Ryan L. Collins, Jackie LoPiccolo, and the Gusev/Van Allen Laboratories
 # Distributed under terms of the GNU GPL v2.0 License (see LICENSE)
 # Contact: Ryan L. Collins <Ryan_Collins@dfci.harvard.edu>
 
@@ -20,8 +20,8 @@
 ### Set local parameters
 export TCGADIR=/data/gusev/USERS/rlc47/TCGA
 export PROFILEDIR=/data/gusev/USERS/rlc47/PROFILE
-export WRKDIR=/data/gusev/USERS/rlc47/RAS_modifier_analysis
-export CODEDIR=$WRKDIR/../code/ras_modifiers
+export WRKDIR=/data/gusev/USERS/rlc47/EGFR_modifier_analysis
+export CODEDIR=$WRKDIR/../EGFR_modifier_analysis/code/ras_modifiers
 export VEP_CACHE=$WRKDIR/../refs/vep_cache
 export VEP_PLUGINS=$WRKDIR/../code/vep_plugins
 cd $WRKDIR
@@ -33,250 +33,10 @@ for SUBDIR in LSF LSF/logs LSF/scripts; do
     mkdir $WRKDIR/$SUBDIR
   fi
 done
-if ! [ -e $VEP_CACHE ]; then
-  mkdir $VEP_CACHE
-fi
-if ! [ -e $VEP_PLUGINS ]; then
-  mkdir $VEP_PLUGINS
-fi
 
 
-### Download & install VEP and associated plugins
-# Prep directory structure
-cd $WRKDIR/../code/ && \
-git clone https://github.com/Ensembl/ensembl-vep.git && \
-cd ensembl-vep && \
-perl INSTALL.pl \
-  --NO_HTSLIB \
-  --CONVERT \
-  --CACHEDIR $VEP_CACHE/ \
-  --PLUGINSDIR $VEP_PLUGINS/ && \
-cd $WRKDIR
-# Download GRCh37 & GRCh38 indexed VEP caches
-for ref in GRCh37 GRCh38; do
-  bsub \
-    -q normal -J ${ref}_VEP_cache_download \
-    -o $WRKDIR/LSF/logs/${ref}_VEP_cache_download.log \
-    -e $WRKDIR/LSF/logs/${ref}_VEP_cache_download.err \
-    "cd $VEP_CACHE && \
-     wget https://ftp.ensembl.org/pub/release-108/variation/indexed_vep_cache/homo_sapiens_vep_108_${ref}.tar.gz && \
-     tar -xzf homo_sapiens_vep_108_${ref}.tar.gz"
-done
-# Test VEP installation
-cd $WRKDIR/../code/ensembl-vep && \
-./vep \
-  -i examples/homo_sapiens_GRCh37.vcf \
-  --cache \
-  --dir_cache $VEP_CACHE/ \
-  --cache_version 108 \
-  --offline && \
-cd $WRKDIR
-
-
-#####################
-### Data curation ###
-#####################
-
-### Curate custom datasets for use by VEP
-# Prep subdirectories
-for subdir in UTRAnnotator gnomad loftee CADD clinvar cosmic SpliceAI; do
-  if ! [ -e $VEP_CACHE/$subdir ]; then
-    mkdir $VEP_CACHE/$subdir
-  fi
-done
-# dbNSFP
-bsub \
-  -q normal -J dbNSFP_cache_download \
-  -o $WRKDIR/LSF/logs/dbNSFP_cache_download.log \
-  -e $WRKDIR/LSF/logs/dbNSFP_cache_download.err \
-  "cd $VEP_CACHE && \
-   wget ftp://dbnsfp:dbnsfp@dbnsfp.softgenetics.com/dbNSFP4.3a.zip && \
-   unzip dbNSFP4.3a.zip"
-zcat $VEP_CACHE/dbNSFP4.3a_variant.chr1.gz \
-| head -n1 > $TMPDIR/dbNSFP_header
-cat <<EOF > $WRKDIR/LSF/scripts/prep_dbNSFP.grch38.sh
-#!/usr/bin/env bash
-. /PHShome/rlc47/.bashrc
-cd $WRKDIR
-zgrep -h -v ^#chr $VEP_CACHE/dbNSFP4.3a_variant.chr* \
-| sort -T $TMPDIR -k1,1 -k2,2n - | cat $TMPDIR/dbNSFP_header - \
-| bgzip -c > $VEP_CACHE/dbNSFP4.3a_grch38.gz
-tabix -s 1 -b 2 -e 2 -f $VEP_CACHE/dbNSFP4.3a_grch38.gz
-EOF
-cat <<EOF > $WRKDIR/LSF/scripts/prep_dbNSFP.grch37.sh
-#!/usr/bin/env bash
-. /PHShome/rlc47/.bashrc
-cd $WRKDIR
-zgrep -h -v ^#chr $VEP_CACHE/dbNSFP4.3a_variant.chr* \
-| awk '\$8 != "." ' | sort -T $TMPDIR -k8,8 -k9,9n - | cat $TMPDIR/dbNSFP_header - \
-| bgzip -c > $VEP_CACHE/dbNSFP4.3a_grch37.gz
-tabix -s 8 -b 9 -e 9 -f $VEP_CACHE/dbNSFP4.3a_grch37.gz
-EOF
-for ref in 37 38; do
-  script=$WRKDIR/LSF/scripts/prep_dbNSFP.grch$ref.sh
-  chmod a+x $script
-  rm $WRKDIR/LSF/logs/prep_dbNSFP.grch$ref.*
-  bsub \
-    -q normal -J prep_dbNSFP.grch$ref \
-    -o $WRKDIR/LSF/logs/prep_dbNSFP.grch$ref.log \
-    -e $WRKDIR/LSF/logs/prep_dbNSFP.grch$ref.err \
-    $WRKDIR/LSF/scripts/prep_dbNSFP.grch$ref.sh
-done
-# LOFTEE
-for file in $VEP_PLUGINS/loftee/*; do
-  ln -fs $( echo -e $file ) $VEP_PLUGINS/$( basename $file )
-done
-for suffix in gz gz.fai gz.gzi; do
-  bsub \
-    -q normal -J dl_loftee_ancestor_fa$suffix \
-    -o $WRKDIR/LSF/logs/dl_loftee_ancestor_fa$suffix.log \
-    -e $WRKDIR/LSF/logs/dl_loftee_ancestor_fa$suffix.err \
-    "cd $VEP_CACHE/loftee && \
-     wget https://s3.amazonaws.com/bcbio_nextgen/human_ancestor.fa.$suffix"
-done
-bsub \
-  -q normal -J dl_loftee_conservation_db \
-  -o $WRKDIR/LSF/logs/dl_loftee_conservation_db.log \
-  -e $WRKDIR/LSF/logs/dl_loftee_conservation_db.err \
-  "cd $VEP_CACHE/loftee && \
-   wget https://personal.broadinstitute.org/konradk/loftee_data/GRCh37/phylocsf_gerp.sql.gz && \
-   gunzip phylocsf_gerp.sql.gz"
-# CADD
-for prefix in whole_genome_SNVs InDels; do
-  for suffix in gz gz.tbi; do
-    bsub \
-      -q normal -J dl_cadd_$prefix.tsv.$suffix \
-      -o $WRKDIR/LSF/logs/dl_cadd_$prefix.tsv.$suffix.log \
-      -e $WRKDIR/LSF/logs/dl_cadd_$prefix.tsv.$suffix.err \
-      "cd $VEP_CACHE/CADD && \
-       wget https://krishna.gs.washington.edu/download/CADD/v1.6/GRCh37/$prefix.tsv.$suffix"
-  done
-done
-# ClinVar
-wget \
-  -P $VEP_CACHE/clinvar/ \
-  https://ftp.ncbi.nlm.nih.gov/pub/clinvar/vcf_GRCh37/clinvar.vcf.gz \
-  https://ftp.ncbi.nlm.nih.gov/pub/clinvar/vcf_GRCh37/clinvar.vcf.gz.tbi
-# COSMIC
-# Note: first need to generate a COSMIC command line access key
-# See https://cancer.sanger.ac.uk/cosmic/download for instructions
-# Need to assign this to variable $my_access_code for these commands to work
-curl \
-  -H "Authorization: Basic $my_access_code" \
-  https://cancer.sanger.ac.uk/cosmic/file_download/GRCh38/cosmic/v97/CMC.tar \
-| jq .url | tr -d '"' \
-| xargs -I {} wget --no-check-certificate -P $TMPDIR/ {}
-mkdir $TMPDIR/cosmic/ && tar -xvf $TMPDIR/CMC.tar* -C $TMPDIR/cosmic/
-bsub \
-  -q normal -J cmc_to_vcf \
-  -o $WRKDIR/LSF/logs/cmc_to_vcf.log \
-  -e $WRKDIR/LSF/logs/cmc_to_vcf.err \
-  "$CODEDIR/scripts/data_processing/cmc_to_vcf.py \
-     --cmc $TMPDIR/cosmic/cmc_export.tsv.gz \
-     --header $WRKDIR/../refs/simple_hg19_header.somatic.vcf.gz \
-     --ref-fasta $TCGADIR/refs/GRCh37.fa \
-   | bcftools sort -O z -o $VEP_CACHE/cosmic/cmc.vcf.gz && \
-   tabix -p vcf -f $VEP_CACHE/cosmic/cmc.vcf.gz"
-# ABC
-wget -P $TMPDIR/ \
-  ftp://ftp.broadinstitute.org/outgoing/lincRNA/ABC/AllPredictions.AvgHiC.ABC0.015.minus150.ForABCPaperV3.txt.gz
-$CODEDIR/scripts/data_processing/prep_ABC.py \
-  --abc $TMPDIR/AllPredictions.AvgHiC.ABC0.015.minus150.ForABCPaperV3.txt.gz \
-| sort -Vk1,1 -k2,2n -k3,3n | bgzip -c \
-> $VEP_CACHE/ABC_enhancers.bed.gz
-tabix -p bed -f $VEP_CACHE/ABC_enhancers.bed.gz
-# gnomAD
-for subset in exomes genomes; do
-  bsub \
-    -q normal -J gnomad_${subset}_cache_download \
-    -o $WRKDIR/LSF/logs/gnomad_${subset}_cache_download.log \
-    -e $WRKDIR/LSF/logs/gnomad_${subset}_cache_download.err \
-    "cd $VEP_CACHE/gnomad && \
-     wget https://ftp.ensembl.org/pub/data_files/homo_sapiens/GRCh37/variation_genotype/gnomad.$subset.r2.0.1.sites.noVEP.vcf.gz && \
-     wget https://ftp.ensembl.org/pub/data_files/homo_sapiens/GRCh37/variation_genotype/gnomad.$subset.r2.0.1.sites.noVEP.vcf.gz.tbi"
-done
-# Gene promoters (defined as +2kb upstream of TSS)
-$CODEDIR/scripts/data_processing/make_promoters.py \
-  --gtf $WRKDIR/../refs/gencode.v19.annotation.gtf.gz \
-  --coding-only \
-| sort -Vk1,1 -k2,2n -k3,3n | bgzip -c \
-> $VEP_CACHE/gencode_promoters.bed.gz
-tabix -p bed -f $VEP_CACHE/gencode_promoters.bed.gz
-# CCRs
-wget -P $TMPDIR/ \
-  https://s3.us-east-2.amazonaws.com/ccrs/ccrs/ccrs.autosomes.v2.20180420.bed.gz \
-  https://s3.us-east-2.amazonaws.com/ccrs/ccrs/ccrs.xchrom.v2.20180420.bed.gz
-zcat \
-  $TMPDIR/ccrs.autosomes.v2.20180420.bed.gz \
-  $TMPDIR/ccrs.xchrom.v2.20180420.bed.gz \
-| awk -v OFS="\t" '{ print $1, $2, $3, $5":"$4 }' \
-| grep -ve '^#' | sort -Vk1,1 -k2,2n -k3,3 | bgzip -c \
-> $VEP_CACHE/CCR.bed.gz
-tabix -f -p bed $VEP_CACHE/CCR.bed.gz
-# GERP, phastCons, phyloP
-cat << EOF > $TMPDIR/conservation_bw_urls.list
-http://hgdownload.soe.ucsc.edu/gbdb/hg19/bbi/All_hg19_RS.bw
-http://hgdownload.soe.ucsc.edu/goldenPath/hg19/phastCons100way/hg19.100way.phastCons.bw
-http://hgdownload.soe.ucsc.edu/goldenPath/hg19/phyloP100way/hg19.100way.phyloP100way.bw
-EOF
-while read url; do
-  bsub \
-    -q normal -J dl_$( basename $url ) \
-    -o $WRKDIR/LSF/logs/dl_$( basename $url ).log \
-    -e $WRKDIR/LSF/logs/dl_$( basename $url ).err \
-    "wget -P $VEP_CACHE/ $url"
-done < $TMPDIR/conservation_bw_urls.list
-while read url; do
-  old=$VEP_CACHE/$( basename $url )
-  new=$( echo $old | sed 's/hg19/GRCh37/g' )
-  bsub \
-    -q normal -J convert_$( basename $old )_contigs \
-    -o $WRKDIR/LSF/logs/convert_$( basename $old )_contigs.log \
-    -e $WRKDIR/LSF/logs/convert_$( basename $old )_contigs.err \
-    "$CODEDIR/scripts/data_processing/convert_bigwig_contigs.py $old $new"
-done < $TMPDIR/conservation_bw_urls.list
-# GTEx eQTL
-bsub \
-  -q normal -J dl_GTEx_eQTL \
-  -o $WRKDIR/LSF/logs/dl_GTEx_eQTL.log \
-  -e $WRKDIR/LSF/logs/dl_GTEx_eQTL.err \
-  "wget -P $TMPDIR/ https://storage.googleapis.com/gtex_analysis_v7/single_tissue_eqtl_data/GTEx_Analysis_v7_eQTL.tar.gz"
-tar -xzf $TMPDIR/GTEx_Analysis_v7_eQTL.tar.gz -C $TMPDIR/
-$CODEDIR/scripts/data_processing/gtex_eqtl_to_vcf.py \
-  --gtex-tsv $TMPDIR/GTEx_Analysis_v7_eQTL/Pancreas.v7.signif_variant_gene_pairs.txt.gz \
-  --gtex-tsv $TMPDIR/GTEx_Analysis_v7_eQTL/Skin_Sun_Exposed_Lower_leg.v7.signif_variant_gene_pairs.txt.gz \
-  --gtex-tsv $TMPDIR/GTEx_Analysis_v7_eQTL/Skin_Not_Sun_Exposed_Suprapubic.v7.signif_variant_gene_pairs.txt.gz \
-  --gtex-tsv $TMPDIR/GTEx_Analysis_v7_eQTL/Colon_Sigmoid.v7.signif_variant_gene_pairs.txt.gz \
-  --gtex-tsv $TMPDIR/GTEx_Analysis_v7_eQTL/Colon_Transverse.v7.signif_variant_gene_pairs.txt.gz \
-  --gtex-tsv Lung.v7.signif_variant_gene_pairs.txt.gz \
-  --header $WRKDIR/../refs/simple_hg19_header.somatic.vcf.gz \
-| bcftools sort -O z -o $VEP_CACHE/GTEx_eQTLs.vcf.gz
-tabix -p vcf -f $VEP_CACHE/GTEx_eQTLs.vcf.gz
-# Gencode map of ENSG:symbol:ENST:CDS length
-cat <<EOF > $WRKDIR/LSF/scripts/build_transcript_table.sh
-#!/usr/bin/env bash
-$CODEDIR/scripts/data_processing/build_gencode_table.py \
-  --gtf $WRKDIR/../refs/gencode.v19.annotation.gtf.gz \
-  --outfile $TMPDIR/gencode.v19.annotation.transcript_info.unsorted.tsv \
-  --no-header
-
-sort -Vk3,3 -k2,2V -k1,1V -k4,4n \
-  $TMPDIR/gencode.v19.annotation.transcript_info.unsorted.tsv \
-| gzip -c > \
-$WRKDIR/../refs/gencode.v19.annotation.transcript_info.tsv.gz
-EOF
-chmod a+x $WRKDIR/LSF/scripts/build_transcript_table.sh
-bsub \
-  -q normal -J build_transcript_table \
-  -o $WRKDIR/LSF/logs/build_transcript_table.log \
-  -e $WRKDIR/LSF/logs/build_transcript_table.err \
-  $WRKDIR/LSF/scripts/build_transcript_table.sh
-# SpliceAI
-for suf in gz gz.tbi; do
-  ln -s \
-    /data/talkowski/dg520/ref/spliceai/spliceai_scores.raw.snv.hg19.vcf.$suf \
-    $VEP_CACHE/SpliceAI/spliceai_scores.raw.snv.hg37.vcf.$suf
-done
+### NOTE: EXPECTS FULL RAS MODIFIER DATA CURATION TO BE ALREADY COMPLETE 
+### SEE VERSION OF THIS FILE IN THE MAIN BRANCH
 
 
 ######################
@@ -360,7 +120,7 @@ for cohort in TCGA PROFILE; do
       COHORTDIR=$PROFILEDIR
       ;;
   esac
-  for subset in somatic_variants RAS_loci; do
+  for subset in EGFR_loci; do
     for suf in err log; do
       if [ -e $WRKDIR/LSF/logs/VEP_${cohort}_$subset.$suf ]; then
         rm $WRKDIR/LSF/logs/VEP_${cohort}_$subset.$suf
@@ -409,7 +169,7 @@ for cohort in TCGA PROFILE; do
       COHORTDIR=$PROFILEDIR
       ;;
   esac
-  for subset in somatic_variants RAS_loci; do
+  for subset in EGFR_loci; do
     for suf in err log; do
       if [ -e $WRKDIR/LSF/logs/VEP_cleanup_${cohort}_$subset.$suf ]; then
         rm $WRKDIR/LSF/logs/VEP_cleanup_${cohort}_$subset.$suf
@@ -457,7 +217,7 @@ for cohort in TCGA PROFILE; do
       sample_field=PBP
       ;;
   esac
-  for subset in somatic_variants RAS_loci; do
+  for subset in EGFR_loci; do
     for suf in err log; do
       if [ -e $WRKDIR/LSF/logs/annotate_AFs_${cohort}_$subset.$suf ]; then
         rm $WRKDIR/LSF/logs/annotate_AFs_${cohort}_$subset.$suf
@@ -469,7 +229,7 @@ for cohort in TCGA PROFILE; do
       -e $WRKDIR/LSF/logs/annotate_AFs_${cohort}_$subset.err \
       "$WRKDIR/LSF/scripts/annotate_AFs.sh \
          $COHORTDIR/data/$cohort.$subset.anno.clean.vcf.gz \
-         $COHORTDIR/data/sample_info/$cohort.ALL.sample_metadata.tsv.gz \
+         $COHORTDIR/data/sample_info/$cohort.LUAD.sample_metadata.tsv.gz \
          $sample_field \
          $COHORTDIR/data/$cohort.$subset.anno.clean.wAF.vcf.gz"
   done
@@ -490,7 +250,7 @@ for cohort in TCGA PROFILE; do
       sample_field=PBP
       ;;
   esac
-  for subset in somatic_variants RAS_loci; do
+  for subset in EGFR_loci; do
     for suf in err log; do
       if [ -e $WRKDIR/LSF/logs/make_dosage_${cohort}_$subset.$suf ]; then
         rm $WRKDIR/LSF/logs/make_dosage_${cohort}_$subset.$suf
