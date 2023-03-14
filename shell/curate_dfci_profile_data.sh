59b59e22596e0058b3fc6dc349137b575025a86d
Ryan Collins <rlcollins@g.harvard.edu>
2023-03-13 21:50:20 -0400

    Curate germline data for PROFILE and TCGA

--- shell/curate_dfci_profile_data.sh
+++ shell/curate_dfci_profile_data.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
-###############################
-#    RAS Modifiers Project    #
-###############################
+################################
+#    EGFR Modifiers Project    #
+################################
 
-# Copyright (c) 2022-Present Ryan L. Collins and the Van Allen/Gusev/Haigis Laboratories
+# Copyright (c) 2023-Present Ryan L. Collins, Jackie LoPiccolo, and the Gusev/Van Allen Laboratories
 # Distributed under terms of the GNU GPL v2.0 License (see LICENSE)
 # Contact: Ryan L. Collins <Ryan_Collins@dfci.harvard.edu>
 
-# Curate data from DFCI-PROFILE cohort for RAS modifiers study
+# Curate data from DFCI-PROFILE cohort for EGFR modifiers study
 
 # Note: intended to be executed on the MGB ERISOne cluster
 
@@ -18,44 +18,14 @@ export BASEDIR=/data/gusev/PROFILE
 export GTDIR=$BASEDIR/2020_2022_combined/IMPUTE_HQ
 export CLINDIR=$BASEDIR/CLINICAL
 export WRKDIR=/data/gusev/USERS/rlc47/PROFILE
-export CODEDIR=$WRKDIR/../code/ras_modifiers
+export CODEDIR=$WRKDIR/../EGFR_modifier_analysis/code/ras_modifiers
 cd $WRKDIR
 
 
-### Set up directory trees as necessary
-for SUBDIR in data data/sample_info LSF LSF/scripts LSF/logs ../refs refs; do
-  if ! [ -e $WRKDIR/$SUBDIR ]; then
-    mkdir $WRKDIR/$SUBDIR
-  fi
-done
+### NOTE: EXPECTS FULL RAS MODIFIER DATA CURATION TO BE ALREADY COMPLETE 
+### SEE VERSION OF THIS FILE IN THE MAIN BRANCH
 
 
-### Curate clinical information for patients of interest
-# Get list of all IDs present in VCF
-tabix -H $GTDIR/PROFILE_COMB.22.HQ.vcf.gz \
-| fgrep -v "##" | cut -f10- | sed 's/\t/\n/g' \
-> $WRKDIR/data/sample_info/PROFILE.vcf.samples.list
-# Curate EHR and get list of patients from cancer types of interest
-$CODEDIR/scripts/data_processing/preprocess_dfci_profile_ehr.py \
-  --id-map-tsv $CLINDIR/PROFILE_MRN_BL_PANEL.PBP.tab \
-  --genomic-csv $CLINDIR/OncDRS/ALL_2021_11/GENOMIC_SPECIMEN.csv.gz \
-  --dx-csv $CLINDIR/OncDRS/ALL_2021_11/CANCER_DIAGNOSIS_CAREG.csv.gz \
-  --ancestry-csv $CLINDIR/PROFILE_2022_ANCESTRY.csv.gz \
-  --hx-csv $CLINDIR/OncDRS/ALL_2021_11/HEALTH_HISTORY.csv.gz \
-  --survival-csv $CLINDIR/OncDRS/ALL_2021_11/PT_INFO_STATUS_REGISTRATION.csv.gz \
-  --out-prefix $WRKDIR/data/sample_info/PROFILE. \
-  --vcf-ids $WRKDIR/data/sample_info/PROFILE.vcf.samples.list
-
-
-### Subset VCFs to patients of interest and RAS loci
-# # Recompress chromosomes 1-3 with bgzip & reindex
-# for contig in $( seq 1 3 ); do
-#   bsub -R 'rusage[mem=8000]' -q long -J compress_index_$contig \
-#     "cd /data/gusev/PROFILE/2020_2022_combined/IMPUTE_HQ; \
-#      . /PHShome/rlc47/.bashrc; \
-#      zcat PROFILE_COMB.$contig.HQ.vcf.gz | bgzip -c > PROFILE_COMB.$contig.HQ.vcf.bgz; \
-#      bcftools index PROFILE_COMB.$contig.HQ.vcf.bgz"
-# done
 # Extract samples & loci of interest
 while read contig start end gene; do
   cat << EOF > $WRKDIR/LSF/scripts/extract_${gene}_variants.sh
@@ -65,7 +35,7 @@ cd $WRKDIR
 bcftools view \
   -O z -o $WRKDIR/data/PROFILE.$gene.vcf.gz \
   --min-ac 1 \
-  --samples-file $WRKDIR/data/sample_info/PROFILE.ALL.samples.list \
+  --samples-file $WRKDIR/data/sample_info/PROFILE.LUAD.samples.list \
   --regions "$contig:${start}-$end" \
   $GTDIR/PROFILE_COMB.$contig.HQ.vcf.gz
 tabix -p vcf -f $WRKDIR/data/PROFILE.$gene.vcf.gz
@@ -77,75 +47,25 @@ EOF
     -o $WRKDIR/LSF/logs/extract_${gene}_variants.log \
     -e $WRKDIR/LSF/logs/extract_${gene}_variants.err \
     $WRKDIR/LSF/scripts/extract_${gene}_variants.sh
-done < <( zcat $CODEDIR/refs/RAS_loci.GRCh37.bed.gz | fgrep -v "#" )
+done < <( zcat $CODEDIR/refs/EGFR_loci.GRCh37.bed.gz | fgrep -v "#" )
 # Merge VCFs for each gene into a single VCF and index the merged VCF
-zcat $CODEDIR/refs/RAS_loci.GRCh37.bed.gz | fgrep -v "#" | cut -f4 \
-| xargs -I {} echo "$WRKDIR/data/PROFILE.{}.vcf.gz" \
-> $WRKDIR/data/PROFILE.single_gene_vcfs.list
-bcftools concat \
-  --file-list $WRKDIR/data/PROFILE.single_gene_vcfs.list \
-| bcftools annotate \
+bcftools annotate \
   --set-id '%CHROM\_%POS\_%REF\_%FIRST_ALT' \
   --header-lines <( zcat $WRKDIR/refs/simple_hg19_header.vcf.gz | fgrep -w SVTYPE ) \
-  -O z -o $WRKDIR/data/PROFILE.RAS_loci.vcf.gz
-tabix -p vcf -f $WRKDIR/data/PROFILE.RAS_loci.vcf.gz
-
-
-### Curate somatic data for patients of interest
-# Download Gencode v19 GTF for extracting gene coordinates
-wget -O /dev/stdout \
- https://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_19/gencode.v19.annotation.gtf.gz \
-| gunzip -c | grep -ve '^#' | sort -Vk1,1 -k4,4n -k5,5n | bgzip -c \
-> $WRKDIR/../refs/gencode.v19.annotation.gtf.gz
-tabix -f -p gff $WRKDIR/../refs/gencode.v19.annotation.gtf.gz
-# Write list of donors missing somatic data
-$CODEDIR/scripts/data_processing/find_dfci_profile_missing_somatic.py \
-  --mutation-csv $CLINDIR/OncDRS/ALL_2021_11/GENOMIC_MUTATION_RESULTS.csv.gz \
-  --cna-csv $CLINDIR/OncDRS/ALL_2021_11/GENOMIC_CNV_RESULTS.csv.gz \
-  --samples-list $WRKDIR/data/sample_info/PROFILE.ALL.samples.list \
-  --id-map-tsv $CLINDIR/PROFILE_MRN_BL_PANEL.PBP.tab \
-  --out-prefix $WRKDIR/data/sample_info/PROFILE.ALL.samples.missing_somatic
-# Curate somatic data
-$CODEDIR/scripts/data_processing/preprocess_dfci_profile_somatic.py \
-  --mutation-csv $CLINDIR/OncDRS/ALL_2021_11/GENOMIC_MUTATION_RESULTS.csv.gz \
-  --cna-csv $CLINDIR/OncDRS/ALL_2021_11/GENOMIC_CNV_RESULTS.csv.gz \
-  --samples-list $WRKDIR/data/sample_info/PROFILE.ALL.samples.list \
-  --no-mutation-data $WRKDIR/data/sample_info/PROFILE.ALL.samples.missing_somatic.mut.list \
-  --no-cna-data $WRKDIR/data/sample_info/PROFILE.ALL.samples.missing_somatic.cna.list \
-  --id-map-tsv $CLINDIR/PROFILE_MRN_BL_PANEL.PBP.tab \
-  --genes-gtf $WRKDIR/../refs/gencode.v19.annotation.gtf.gz \
-  --ref-fasta $WRKDIR/refs/GRCh37.fa \
-  --header $WRKDIR/../refs/simple_hg19_header.somatic.vcf.gz \
-  --outfile $WRKDIR/data/PROFILE.somatic_variants.vcf.gz \
-  --out-tsv $WRKDIR/data/PROFILE.somatic_variants.tsv.gz
-tabix -p vcf -f $WRKDIR/data/PROFILE.somatic_variants.vcf.gz
+  -O z -o $WRKDIR/data/PROFILE.EGFR_loci.vcf.gz \
+  $WRKDIR/data/PROFILE.EGFR.vcf.gz
+tabix -p vcf -f $WRKDIR/data/PROFILE.EGFR_loci.vcf.gz
 
 
 # Summarize somatic variant status by gene & cancer type
-for cancer in PDAC CRAD LUAD SKCM; do
-  n_samp=$( fgrep -wvf \
-              $WRKDIR/data/sample_info/PROFILE.ALL.samples.missing_somatic.list \
-              ${WRKDIR}/data/sample_info/PROFILE.$cancer.samples.list | wc -l )
-  while read gene; do
-    zcat $WRKDIR/data/PROFILE.somatic_variants.tsv.gz \
-    | fgrep -wf ${WRKDIR}/data/sample_info/PROFILE.$cancer.samples.list \
-    | fgrep -wvf $WRKDIR/data/sample_info/PROFILE.ALL.samples.missing_somatic.list \
-    | awk -v gene=$gene -v FS="\t" '{ if ($9==gene && ($15~/AMP|DEL|Frame_Shift_Del|Frame_Shift_Ins|In_Frame_Del|In_Frame_Ins|Missense_Mutation|Nonsense_Mutation|Nonstop_Mutation|Splice_Site/)) print $7 }' \
-    | sort | uniq | wc -l | awk -v n=$n_samp '{ print $1/n }'
-  done < <( zcat $CODEDIR/refs/RAS_loci.GRCh37.bed.gz | fgrep -v "#" | cut -f4 )
+n_samp=$( fgrep -wvf \
+            $WRKDIR/data/sample_info/PROFILE.ALL.samples.missing_somatic.list \
+            ${WRKDIR}/data/sample_info/PROFILE.LUAD.samples.list | wc -l )
+while read gene; do
   zcat $WRKDIR/data/PROFILE.somatic_variants.tsv.gz \
-  | fgrep -wf ${WRKDIR}/data/sample_info/PROFILE.$cancer.samples.list \
+  | fgrep -wf ${WRKDIR}/data/sample_info/PROFILE.LUAD.samples.list \
   | fgrep -wvf $WRKDIR/data/sample_info/PROFILE.ALL.samples.missing_somatic.list \
-  | awk -v FS="\t" '{ if ($9~/NRAS|HRAS|KRAS/ && ($15~/AMP|DEL|Frame_Shift_Del|Frame_Shift_Ins|In_Frame_Del|In_Frame_Ins|Missense_Mutation|Nonsense_Mutation|Nonstop_Mutation|Splice_Site/)) print $7 }' \
+  | awk -v gene=$gene -v FS="\t" '{ if ($9==gene && ($15~/AMP|DEL|Frame_Shift_Del|Frame_Shift_Ins|In_Frame_Del|In_Frame_Ins|Missense_Mutation|Nonsense_Mutation|Nonstop_Mutation|Splice_Site/)) print $7 }' \
   | sort | uniq | wc -l | awk -v n=$n_samp '{ print $1/n }'
-done | paste - - - -
-
-
-# Curate selected PRS for PROFILE samples
-$CODEDIR/scripts/data_processing/curate_profile_prs.R \
-  $BASEDIR/23andme/DFCI_PRS_scores.txt \
-  $WRKDIR/data/sample_info/PROFILE.ALL.samples.list \
-  $CODEDIR/refs/PROFILE_selected_PRS.tsv \
-  $WRKDIR/data/PROFILE.PRS.tsv
-gzip -f $WRKDIR/data/PROFILE.PRS.tsv
+done < <( zcat $CODEDIR/refs/EGFR_loci.GRCh37.bed.gz | fgrep -v "#" | cut -f4 )
 
--- shell/curate_tcga_data_erisone.sh
+++ shell/curate_tcga_data_erisone.sh
@@ -8,7 +8,7 @@
 # Distributed under terms of the GNU GPL v2.0 License (see LICENSE)
 # Contact: Ryan L. Collins <Ryan_Collins@dfci.harvard.edu>
 
-# Curate (some) data from TCGA cohort for RAS modifiers study
+# Curate (some) data from TCGA cohort for EGFR modifiers study
 
 # Note: intended to be executed on the MGB ERISOne cluster
 
@@ -25,7 +25,7 @@ cd $WRKDIR
 ### SEE VERSION OF THIS FILE IN THE MAIN BRANCH
 
 
-### Subset VCFs to patients of interest and RAS loci
+### Subset VCFs to patients of interest and EGFR loci
 # Extract samples & loci of interest from genotyped and imputed arrays
 while read contig start end gene; do
   for tech in array_typed array_imputed; do
@@ -46,7 +46,7 @@ cd $WRKDIR
 bcftools view \
   -O z -o $WRKDIR/data/TCGA.$gene.$tech.vcf.gz \
   $bcftools_options \
-  --samples-file $WRKDIR/data/sample_info/TCGA.ALL.$tech.samples.list \
+  --samples-file $WRKDIR/data/sample_info/TCGA.LUAD.$tech.samples.list \
   --regions "$contig:${start}-$end" \
   $VCF
 tabix -p vcf -f $WRKDIR/data/TCGA.$gene.$tech.vcf.gz
@@ -69,9 +69,6 @@ while read contig start end gene; do
   tabix -p bed -f $WRKDIR/refs/TCGA_WES.covered_intervals.$gene.bed.gz
 done < <( zcat $CODEDIR/refs/EGFR_loci.GRCh37.bed.gz | fgrep -v "#" )
 # Use bcftools to stream WES data from gs:// bucket for samples & loci of interest
-gsutil -m cp \
-  gs://terra-workspace-archive-lifecycle/fc-e5ae96e4-c495-44d1-9155-b27057d570d8/e3d12a6b-5051-4656-b911-ea425aa14ce7/VT_Decomp/49610c65-a66c-45e9-9ef3-a3b5ab80ac9f/call-VTRecal/all_normal_samples.vt2_normalized_spanning_alleles.vcf.gz.tbi \
-  $WRKDIR/data/
 gcloud auth application-default login
 export GCS_OAUTH_TOKEN=`gcloud auth application-default print-access-token`
 cd $WRKDIR/data
@@ -79,7 +76,7 @@ while read contig start end gene; do
   bcftools view \
     -O z -o $WRKDIR/data/TCGA.$gene.exome.vcf.gz \
     --min-ac 1 \
-    --samples-file $WRKDIR/data/sample_info/TCGA.ALL.exome.samples.list \
+    --samples-file $WRKDIR/data/sample_info/TCGA.LUAD.exome.samples.list \
     --regions-file $WRKDIR/refs/TCGA_WES.covered_intervals.$gene.bed.gz \
     gs://terra-workspace-archive-lifecycle/fc-e5ae96e4-c495-44d1-9155-b27057d570d8/e3d12a6b-5051-4656-b911-ea425aa14ce7/VT_Decomp/49610c65-a66c-45e9-9ef3-a3b5ab80ac9f/call-VTRecal/all_normal_samples.vt2_normalized_spanning_alleles.vcf.gz
   tabix -p vcf -f $WRKDIR/data/TCGA.$gene.exome.vcf.gz
@@ -109,79 +106,22 @@ EOF
       -e $WRKDIR/LSF/logs/merge_arrays_exomes_${gene}.err \
       $WRKDIR/LSF/scripts/merge_arrays_exomes_${gene}.sh
 done < <( zcat $CODEDIR/refs/EGFR_loci.GRCh37.bed.gz | fgrep -v "#" | cut -f4 )
-# Merge VCFs for eacn gene into a single VCF and index the merged VCF
-zcat $CODEDIR/refs/EGFR_loci.GRCh37.bed.gz | fgrep -v "#" | cut -f4 \
-| xargs -I {} echo "$WRKDIR/data/TCGA.{}.merged.vcf.gz" \
-> $WRKDIR/data/TCGA.single_gene_vcfs.list
-bcftools concat \
-  --file-list $WRKDIR/data/TCGA.single_gene_vcfs.list \
-  -O z -o $WRKDIR/data/TCGA.EGFR_loci.vcf.gz
-tabix -p vcf -f $WRKDIR/data/TCGA.EGFR_loci.vcf.gz
-
-
-### Curate somatic data for patients of interest
-# Preprocess mc3 data by subsetting on samples of interest to improve pandas IO speed
-cat \
-  <( zcat $WRKDIR/data/mc3.v0.2.8.PUBLIC.maf.gz | head -n1 ) \
-  <( zcat $WRKDIR/data/mc3.v0.2.8.PUBLIC.maf.gz \
-     | fgrep -wf $WRKDIR/data/sample_info/TCGA.ALL.donors.list ) \
-| gzip -c > $WRKDIR/data/mc3.v0.2.8.PUBLIC.PDAC_CRAD_LUAD_SKCM.maf.gz
-# Pre-filter Affy SNP array CNA calls
-# Note: must have been downloaded by cancer type from GDAC Firehose
-# and uploaded to $WRKDIR/data/TCGA_CNA
-cat $( find $WRKDIR/data/TCGA_CNA -name "*seg.seg.txt" | paste -s ) \
-| fgrep -v "Num_Probes" | awk -v OFS="\t" \
-  '{ cnv="NONE"; if ($6 <= -0.4150375) cnv="DEL"; if ($6 >= 0.3219281) cnv="AMP"; \
-     if ($5>=10 && cnv!="NONE") print $2, $3, $4, cnv, $1 }' \
-| sort -Vk1,1 -k2,2n -k3,3n -k5,5V -k4,4V \
-| cat <( echo -e "#chrom\tstart\tend\tCNA\tsample" ) - | bgzip -c \
-> $WRKDIR/data/TCGA.CNA.b19.bed.gz
-# Write lists of donors with no somatic data available callset
-zcat $WRKDIR/data/mc3.v0.2.8.PUBLIC.PDAC_CRAD_LUAD_SKCM.maf.gz \
-| cut -f16 | cut -f1-3 -d\- | sort | uniq \
-| fgrep -wvf - $WRKDIR/data/sample_info/TCGA.ALL.donors.list \
-> $WRKDIR/data/sample_info/TCGA.ALL.donors.missing_somatic.mc3.list
-cat $( find $WRKDIR/data/TCGA_CNA -name "*seg.seg.txt" | paste -s ) \
-| fgrep -v "Num_Probes" | cut -f1-3 -d\- | sort | uniq \
-| fgrep -wvf - $WRKDIR/data/sample_info/TCGA.ALL.donors.list \
-> $WRKDIR/data/sample_info/TCGA.ALL.donors.missing_somatic.cna.list
-cat \
-  $WRKDIR/data/sample_info/TCGA.ALL.donors.missing_somatic.mc3.list \
-  $WRKDIR/data/sample_info/TCGA.ALL.donors.missing_somatic.cna.list \
-| sort | uniq \
-> $WRKDIR/data/sample_info/TCGA.ALL.donors.missing_somatic.list
-# Curate somatic data
-$CODEDIR/scripts/data_processing/preprocess_tcga_somatic.py \
-  --mc3-tsv $WRKDIR/data/mc3.v0.2.8.PUBLIC.PDAC_CRAD_LUAD_SKCM.maf.gz \
-  --cna-bed $WRKDIR/data/TCGA.CNA.b19.bed.gz \
-  --donors-list $WRKDIR/data/sample_info/TCGA.ALL.donors.list \
-  --no-mutation-data $WRKDIR/data/sample_info/TCGA.ALL.donors.missing_somatic.mc3.list \
-  --no-cna-data $WRKDIR/data/sample_info/TCGA.ALL.donors.missing_somatic.cna.list \
-  --genes-gtf $WRKDIR/../refs/gencode.v19.annotation.gtf.gz \
-  --priority-genes $WRKDIR/../refs/COSMIC.all_GCG.Nov8_2022.genes.list \
-  --ref-fasta $WRKDIR/refs/GRCh37.fa \
-  --header $WRKDIR/../refs/simple_hg19_header.somatic.vcf.gz \
-  --outfile $WRKDIR/data/TCGA.somatic_variants.vcf.gz \
-  --out-tsv $WRKDIR/data/TCGA.somatic_variants.tsv.gz
-tabix -p vcf -f $WRKDIR/data/TCGA.somatic_variants.vcf.gz
+for suffix in gz gz.tbi; do
+  ln -s \
+    $WRKDIR/data/TCGA.EGFR.merged.vcf.$suffix \
+    $WRKDIR/data/TCGA.EGFR_loci.vcf.$suffix
+done
 
 
 # Summarize somatic variant status by gene & cancer type
-for cancer in PDAC CRAD LUAD SKCM; do
-  n_samp=$( fgrep -wvf \
-              $WRKDIR/data/sample_info/TCGA.ALL.donors.missing_somatic.list \
-              ${WRKDIR}/data/sample_info/TCGA.$cancer.donors.list | wc -l )
-  while read gene; do
-    zcat $WRKDIR/data/TCGA.somatic_variants.tsv.gz \
-    | fgrep -wf ${WRKDIR}/data/sample_info/TCGA.$cancer.donors.list \
-    | fgrep -wvf $WRKDIR/data/sample_info/TCGA.ALL.donors.missing_somatic.list \
-    | awk -v gene=$gene -v FS="\t" '{ if ($9==gene && ($15~/AMP|DEL|Frame_Shift_Del|Frame_Shift_Ins|In_Frame_Del|In_Frame_Ins|Missense_Mutation|Nonsense_Mutation|Nonstop_Mutation|Splice_Site/)) print $7 }' \
-    | sort | uniq | wc -l | awk -v n=$n_samp '{ print $1/n }'
-  done < <( zcat $CODEDIR/refs/EGFR_loci.GRCh37.bed.gz | fgrep -v "#" | cut -f4 )
+n_samp=$( fgrep -wvf \
+            $WRKDIR/data/sample_info/TCGA.ALL.donors.missing_somatic.list \
+            ${WRKDIR}/data/sample_info/TCGA.LUAD.donors.list | wc -l )
+while read gene; do
   zcat $WRKDIR/data/TCGA.somatic_variants.tsv.gz \
-    | fgrep -wf ${WRKDIR}/data/sample_info/TCGA.$cancer.donors.list \
-    | fgrep -wvf $WRKDIR/data/sample_info/TCGA.ALL.donors.missing_somatic.list \
-    | awk -v FS="\t" '{ if ($9~/NRAS|HRAS|KRAS/ && $15~/AMP|DEL|Frame_Shift_Del|Frame_Shift_Ins|In_Frame_Del|In_Frame_Ins|Missense_Mutation|Nonsense_Mutation|Nonstop_Mutation|Splice_Site/) print $7 }' \
-    | sort | uniq | wc -l | awk -v n=$n_samp '{ print $1/n }'
-done | paste - - - -
+  | fgrep -wf ${WRKDIR}/data/sample_info/TCGA.LUAD.donors.list \
+  | fgrep -wvf $WRKDIR/data/sample_info/TCGA.ALL.donors.missing_somatic.list \
+  | awk -v gene=$gene -v FS="\t" '{ if ($9==gene && ($15~/AMP|DEL|Frame_Shift_Del|Frame_Shift_Ins|In_Frame_Del|In_Frame_Ins|Missense_Mutation|Nonsense_Mutation|Nonstop_Mutation|Splice_Site/)) print $7 }' \
+  | sort | uniq | wc -l | awk -v n=$n_samp '{ print $1/n }'
+done < <( zcat $CODEDIR/refs/EGFR_loci.GRCh37.bed.gz | fgrep -v "#" | cut -f4 )
 
