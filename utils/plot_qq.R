dfa9563a6538067b38b732846961e28502043f75
Ryan Collins <rlcollins@g.harvard.edu>
2023-03-22 17:20:38 -0400

    Update stupid typo round 2

--- utils/plot_qq.R
+++ utils/plot_qq.R
@@ -41,7 +41,7 @@ parser$add_argument("--p-threshold", metavar="numeric", type="double",
                     help=paste("P-value threshold for significance [default: Bonferroni]"))
 parser$add_argument("--pt-cex", type="double", metavar="float", default=0.35,
                     help=paste("Size scalar for points [default: 0.35]"))
-parser$add_argument("--smallest-p", type="double", metaavar="float", default=1e-20,
+parser$add_argument("--smallest-p", type="double", metavar="float", default=1e-20,
                     help=paste("P-values below this threshold will be rounded [default: 10E-20]"))
 parser$add_argument("--title", matavar="string", type="character", help="Custom plot title")
 args <- parser$parse_args()
