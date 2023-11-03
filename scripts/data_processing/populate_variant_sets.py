9858dc26ac0e1c7e579d453195465c197888d84e
Ryan Collins <ryan.l.collins.13@gmail.com>
2023-11-03 14:05:36 -0400

    Add generic RAS pathway gene list

--- scripts/data_processing/populate_variant_sets.py
+++ scripts/data_processing/populate_variant_sets.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-# Copyright (c) 2022 Ryan L. Collins and the Van Allen/Gusev/Haigis Laboratories
+# Copyright (c) 2022-Present Ryan L. Collins and the Van Allen/Gusev/Haigis Laboratories
 # Distributed under terms of the GPL-2.0 License (see LICENSE)
 # Contact: Ryan L. Collins <Ryan_Collins@dfci.harvard.edu>
 
