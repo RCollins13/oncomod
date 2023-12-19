f969ceae8ccd0624698094a3da4666b7f7ef36b3
Ryan Collins <ryan.l.collins.13@gmail.com>
2023-12-19 09:31:01 -0500

    Make tiny edit to typo in FGFR4 analysis script

--- shell/fgfr4_analysis.sh
+++ shell/fgfr4_analysis.sh
@@ -369,5 +369,5 @@ $CODEDIR/scripts/germline_somatic_assoc/fgfr4_multivariate_model.R \
   --somatic-variant-ids $WRKDIR/data/FGFR4/KRAS_tier_1.somatic.variant_ids.list \
   --germline-haplotype-snps $WRKDIR/plots/germline_somatic_assoc/FGFR4/FGFR4.common_variants.haplotype_assignment.tsv \
   --coding-variant-map $WRKDIR/data/FGFR4/FGFR4.coding_variants.sets.tsv \
-  --out-dir $WRKDIR/plots/germline_somatic_assoc/FGFR4
+  --outdir $WRKDIR/plots/germline_somatic_assoc/FGFR4
 
